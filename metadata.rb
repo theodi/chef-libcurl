ad400f7be4e842b833b2e74ac03121899a5a3c2c
pikesley <github@orgraphone.org>
2013-02-27 11:34:45 +0000

    Simple cookbook to install curl development libs (as required by the curb gem)

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'lib-curl'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Installs/Configures lib-curl'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
